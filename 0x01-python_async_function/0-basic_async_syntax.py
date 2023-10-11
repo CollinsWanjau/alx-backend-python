6b7f0f7cd2676bfb7ac76685bc0e849f82e858d0
colloso999 <tripperskripper@gmail.com>
2023-10-11 22:51:55 +0300

    The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,9 @@
 import asyncio
 import random
 """
-Asynchronous coroutine hat takes in an integer argument 
-(max_delay, with a default value of 10) named wait_random that waits for a 
-random delay between 0 and max_delay (included and float value) seconds 
+Asynchronous coroutine hat takes in an integer argument
+(max_delay, with a default value of 10) named wait_random that waits for a
+random delay between 0 and max_delay (included and float value) seconds
 and eventually returns it.
 """
 
