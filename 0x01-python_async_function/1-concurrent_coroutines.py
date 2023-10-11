5ad83ea96dc91cf4a38c6a6b355f3e52dfe7192e
colloso999 <tripperskripper@gmail.com>
2023-10-12 00:23:37 +0300

    Let's execute multiple coroutines at the same time

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,7 +16,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order
